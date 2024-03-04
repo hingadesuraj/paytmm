78ea154b7e97a5085eb82b861e3888c11f2200ff
Suraj <hingadesuraj@gmail.com>
2024-03-04 18:02:27 +0530

    Create route with parameter and filte user

--- server/routes/user.js
+++ server/routes/user.js
@@ -119,25 +119,49 @@ router.post("/signin", async (req, res) => {
   }
 });
 
-
 // update user info
 
-router.put("/",authMiddleware, async (req,res)=>{
-    const userId = req.userId
-    
-    const checkUserById = await User.findById(userId);
+router.put("/", authMiddleware, async (req, res) => {
+  const userId = req.userId;
 
-    // console.log(checkUserById)
+  const checkUserById = await User.findById(userId);
 
-    const updateUser = await User.findByIdAndUpdate(userId,{
-        password:req.body.password,
-        firstName:req.body.firstName,
-        lastName:req.body.lastName
+  // console.log(checkUserById)
 
+  const updateUser = await User.findByIdAndUpdate(userId, {
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  });
+
+  res.status(200).json({ message: "User information updated..!", updateUser });
+});
+
+// search route using query parameter 
+
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
     })
 
-    res.status(200).json({message:"User information updated..!",updateUser})
-
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
 })
 
 module.exports = router;
