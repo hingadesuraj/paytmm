b97b91824f81d78f64d745aec13ddadd23d1ab6a
Suraj <hingadesuraj@gmail.com>
2024-03-04 17:42:10 +0530

    auth middleware created

---
+++ server/middleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  // get headers
+  const authHeader = req.headers.authorization;
+
+  // check header with bearer or not
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(403).json({});
+  }
+
+  // decode header
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decodeHeader = jwt.verify(token, "surajhingade");
+
+    // pass username or useeid to req body can acces in route for further use
+    req.userId = decodeHeader.userId;
+
+    next();
+  } catch (err) {
+    res.status(411).json({ error: err.message });
+  }
+};
+
+module.exports = authMiddleware;
