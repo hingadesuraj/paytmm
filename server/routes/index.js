56d39326b54c0ec34ef5cf983a00b40ce1d6db41
Suraj <hingadesuraj@gmail.com>
2024-03-06 17:31:55 +0530

    account router created

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const userRouter = require("../routes/user")
+const userRouter = require("../routes/user");
+const accountRouter = require("../routes/account")
 const router = express.Router();
 
 
 
 // all router import heare 
 router.use("/user",userRouter)
+router.use("/account",accountRouter)
 
 module.exports = router; 
