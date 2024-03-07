94c321b500ed31c575c385a032dbc6513a0963d4
Suraj <hingadesuraj@gmail.com>
2024-03-07 12:36:16 +0530

    setup done installing tailwind in react js project

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
   )
 }
 
